2f8438f2336188de38ba96da000d26eadafbc9c5
paulamachadomt <paulamachadomt@gmail.com>
2020-12-11 00:23:58 -0300

    Método que calcula a soma dos valores da diagonal principal de uma matriz bidimensional

---
+++ BecaMatriz/DiagonalPrincipal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BecaMatriz
+{
+    class DiagonalPrincipal
+    {
+        public void CalculaSomaDiagonalPrincipal_Paula()
+        {
+
+            int[,] matriz = new int[,] {
+                { 2, 5, 9 },
+                { 5, 20, 8 },
+                { 8, 12, 16 }
+            };
+
+            int totalSoma = 0;
+
+            for (int linha = 0; linha < matriz.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < matriz.GetLength(0); coluna++)
+                {
+
+                    if (linha == coluna)
+                    {
+                        totalSoma = totalSoma + matriz[linha, coluna];
+                    }
+                }
+            }
+            Console.WriteLine("Soma dos valores da diagonal principal: " + totalSoma);
+            Console.Read();
+        }
+    }
+}
--- BecaMatriz/Program.cs
+++ BecaMatriz/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BecaMatriz
+﻿namespace BecaMatriz
 {
     class Program
     {
@@ -13,6 +7,10 @@ namespace BecaMatriz
             // Cada Becario deverá criar um metódo para calcular a soma da diagonal principal de uma matriz quadrada 3 x 3 
             // O método deverá ter o nome do participante
             // exemplo: CalculaSomaDiagonalPrincipal_Ricardo();
+            
+            DiagonalPrincipal diagonalPrincipal = new DiagonalPrincipal();
+            diagonalPrincipal.CalculaSomaDiagonalPrincipal_Paula();
+
         }
     }
 }
